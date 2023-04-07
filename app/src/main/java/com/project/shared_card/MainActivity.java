6c350d61d77ba6322fa02b7de1880639bd6fe05d
misha <mifa_224@mail.ru>
2023-04-07 18:39:25 +0300

    example

--- app/src/main/java/com/project/shared_card/MainActivity.java
+++ app/src/main/java/com/project/shared_card/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-
+//HELLO
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
