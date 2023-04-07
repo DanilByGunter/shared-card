323f5fb32428db7554b1d701ac1a9e4d2fe252e1
misha <mifa_224@mail.ru>
2023-04-07 19:57:40 +0300

    Misha commit2

--- app/src/main/java/com/project/shared_card/SplashScreenActivity.java
+++ app/src/main/java/com/project/shared_card/SplashScreenActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.WindowManager;
 
 public class SplashScreenActivity extends AppCompatActivity {
-//rdddrsdfdsd
+//rdddrsdfdsd//ddddd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
