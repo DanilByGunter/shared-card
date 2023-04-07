2c6410e962aa4eaef8c51b443ee4ac463a31c364
misha <mifa_224@mail.ru>
2023-04-07 19:46:30 +0300

    Misha commit1

--- app/src/main/java/com/project/shared_card/MainActivity.java
+++ app/src/main/java/com/project/shared_card/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-//HELLO
-    //ddd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
