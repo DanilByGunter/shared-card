0645d92c624d2c7a7fb8215e7453eaeef0b244b4
misha <mifa_224@mail.ru>
2023-04-14 23:57:50 +0300

    кастомизировал поисковую строку и меню сортировки

--- app/src/main/java/com/project/shared_card/activity/main_screen/check/tabs/CurrentListFragment.java
+++ app/src/main/java/com/project/shared_card/activity/main_screen/check/tabs/CurrentListFragment.java
@@ -1,23 +1,28 @@
 package com.project.shared_card.activity.main_screen.check.tabs;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import com.project.shared_card.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CurrentListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class CurrentListFragment extends Fragment {
 
-
+    Button buttonSort;
     public CurrentListFragment() {
     }
 
@@ -31,6 +36,51 @@ public class CurrentListFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        buttonSort = view.findViewById(R.id.button_sort);
+        buttonSort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupMenu();
+            }
+        });
+    }
+    private void popupMenu(){
+        Context wrapper = new ContextThemeWrapper(getContext(), R.style.popup_menu);
+        PopupMenu popupMenu = new PopupMenu(wrapper, buttonSort);
+        popupMenu.inflate(R.menu.popup_menu);
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.by_product:
+                        Toast.makeText(getContext(),
+                                menuItem.getTitle(),
+                                Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.by_date:
+                        Toast.makeText(getContext(),
+                                menuItem.getTitle(),
+                                Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.by_category:
+                        Toast.makeText(getContext(),
+                                menuItem.getTitle(),
+                                Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.by_user:
+                        Toast.makeText(getContext(),
+                                menuItem.getTitle(),
+                                Toast.LENGTH_SHORT).show();
+                    default:
+                        return false;
+                }
+            }
+        });
+        popupMenu.show();
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
--- app/src/main/java/com/project/shared_card/activity/main_screen/check/tabs/TargetListFragment.java
+++ app/src/main/java/com/project/shared_card/activity/main_screen/check/tabs/TargetListFragment.java
@@ -1,17 +1,25 @@
 package com.project.shared_card.activity.main_screen.check.tabs;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import com.project.shared_card.R;
 
 public class TargetListFragment extends Fragment {
-
+    Button buttonSort;
     public TargetListFragment() {
 
     }
@@ -26,7 +34,51 @@ public class TargetListFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        buttonSort = view.findViewById(R.id.button_sort);
+        buttonSort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupMenu();
+            }
+        });
+    }
+    private void popupMenu(){
+        Context wrapper = new ContextThemeWrapper(getContext(), R.style.popup_menu);
+        PopupMenu popupMenu = new PopupMenu(wrapper, buttonSort);
+        popupMenu.inflate(R.menu.popup_menu);
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.by_product:
+                        Toast.makeText(getContext(),
+                                menuItem.getTitle(),
+                                Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.by_date:
+                        Toast.makeText(getContext(),
+                                menuItem.getTitle(),
+                                Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.by_category:
+                        Toast.makeText(getContext(),
+                                menuItem.getTitle(),
+                                Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.by_user:
+                        Toast.makeText(getContext(),
+                                menuItem.getTitle(),
+                                Toast.LENGTH_SHORT).show();
+                    default:
+                        return false;
+                }
+            }
+        });
+        popupMenu.show();
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
