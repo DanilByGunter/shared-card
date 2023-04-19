55558ed19b891440ea37dca88e0344dd16cf926c
misha <mifa_224@mail.ru>
2023-04-19 16:12:23 +0300

    добавлено даилоговое окно

--- app/src/main/java/com/project/shared_card/activity/main_screen/check/CheckFragment.java
+++ app/src/main/java/com/project/shared_card/activity/main_screen/check/CheckFragment.java
@@ -1,6 +1,7 @@
 package com.project.shared_card.activity.main_screen.check;
 
 import android.animation.ValueAnimator;
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 
@@ -18,19 +19,26 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.PopupMenu;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 import com.project.shared_card.R;
+import com.project.shared_card.activity.main_screen.check.dialog.AdapterForSpinner;
 
 
 public class CheckFragment extends Fragment {
 
     ViewPager2 viewPager;
-
+    Dialog dialogCreateProduct;
+    FloatingActionButton buttonAddProduct;
+    Spinner category;
+    Spinner metric;
     public CheckFragment() {
     }
 
@@ -47,7 +55,20 @@ public class CheckFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        String[] metrics = {"пупа", "лупа", "залупа"};
+        String[] categories = {"алкоголь","молочка","мясо"};
+        dialogCreateProduct = new Dialog(getContext());
+        dialogCreateProduct.setContentView(R.layout.dialog_create_product);
+        dialogCreateProduct.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+        metric = dialogCreateProduct.findViewById(R.id.dialog_metric);
+        category = dialogCreateProduct.findViewById(R.id.dialog_text_category);
+        AdapterForSpinner adapterForCategory = new AdapterForSpinner(getContext(),categories);
+        AdapterForSpinner adapterForMetric = new AdapterForSpinner(getContext(),metrics);
+        category.setAdapter(adapterForCategory);
+        metric.setAdapter(adapterForMetric);
         viewPager = view.findViewById(R.id.check_pager);
+        buttonAddProduct = view.findViewById(R.id.add_fab);
         FragmentStateAdapter adapter = new AdapterForPage(getActivity());
         viewPager.setAdapter(adapter);
         viewPager.setOffscreenPageLimit(2);
@@ -62,6 +83,12 @@ public class CheckFragment extends Fragment {
             }
         });
         tabLayoutMediator.attach();
+        buttonAddProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogCreateProduct.show();
+            }
+        });
     }
 
     @Override
